d4112ba3013ae38ab94fb67ae4870d6a319b2965
Damian Jankov <damian.jankov@siemens-healthineers.com>
2024-07-07 14:46:45 +0200

    A simple demo, able to login and message people!

---
+++ BlazorServer/Data/Helpers/LoginService.cs
@@ -0,0 +1,19 @@
+﻿namespace BlazorServer.Data.Helpers;
+
+public class LoginService
+{
+    public bool IsUserLoggedIn { get; private set; }
+    public event Action? OnLoginStatusChanged;
+
+    public void Login()
+    {
+        IsUserLoggedIn = true;
+        OnLoginStatusChanged?.Invoke();
+    }
+
+    public void Logout()
+    {
+        IsUserLoggedIn = false;
+        OnLoginStatusChanged?.Invoke();
+    }
+}
---
+++ BlazorServer/Program.cs
@@ -0,0 +1,41 @@
+using BlazorServer.Data;
+using Microsoft.AspNetCore.ResponseCompression;
+using BlazorServer.Hubs;
+using BlazorServer.Data.Helpers;
+using Microsoft.AspNetCore.Authentication;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddResponseCompression(opts =>
+{
+    opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+        new[] { "application/ocetet-steam" });
+});
+builder.Services.AddSingleton<LoginManager>();
+builder.Services.AddScoped<LoginService>();
+builder.Services.AddScoped<LoginSession>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapHub<ChatHub>("/chathub");
+app.MapFallbackToPage("/_Host");
+
+app.Run();
