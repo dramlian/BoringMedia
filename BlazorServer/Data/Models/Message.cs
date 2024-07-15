b8a509f90211a101223a57375c74e4ac8f8c80a0
Damian Jankov <damian.jankov@siemens-healthineers.com>
2024-07-15 21:57:55 +0200

    The code now saves messages to database and users are able to talk to each other when recipient is offline

--- BlazorServer/Data/Models/Message.cs
+++ BlazorServer/Data/Models/Message.cs
@@ -2,6 +2,7 @@
 {
     public class Message
     {
+        public int Id { get; set; }
         public string recipientUserId { get; set; }
 
         public string senderUserId { get; set; }
@@ -13,14 +14,14 @@
 
         public DateTime time { get; set; }
 
-        public Message(string recipientUserId, string senderUserId, string recipientName, string senderUserName, string message)
+        public Message(string? recipientUserId, string? senderUserId, string recipientName, string senderUserName, string message)
         {
-            this.recipientUserId = recipientUserId;
-            this.senderUserId = senderUserId;
+            this.recipientUserId = recipientUserId ?? string.Empty;
+            this.senderUserId = senderUserId ?? string.Empty;
             this.recipientName = recipientName;
             this.senderUserName = senderUserName;
             this.message = message;
-            this.time = DateTime.Now;
+            this.time = DateTime.UtcNow;
         }
     }
 }
---
+++ BlazorServer/Migrations/20240715184236_InitialCreate.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace BlazorServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MyClasses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    recipientUserId = table.Column<string>(type: "text", nullable: false),
+                    senderUserId = table.Column<string>(type: "text", nullable: false),
+                    recipientName = table.Column<string>(type: "text", nullable: false),
+                    senderUserName = table.Column<string>(type: "text", nullable: false),
+                    message = table.Column<string>(type: "text", nullable: false),
+                    time = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MyClasses", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MyClasses");
+        }
+    }
+}
