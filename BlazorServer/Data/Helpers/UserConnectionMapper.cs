e1b5c64e721f9b8952b3fc4ea213e3aa1231047a
Damian Jankov <damian.jankov@siemens-healthineers.com>
2024-07-21 21:58:05 +0200

    If user is online, it is displayed

--- BlazorServer/Data/Helpers/UserConnectionMapper.cs
+++ BlazorServer/Data/Helpers/UserConnectionMapper.cs
@@ -21,6 +21,11 @@ public class UserConnectionMapper
         else
         {
             nonRegisterdUser.connectionId = connectionId;
+            var existingUser = mappedActiveUsers.FirstOrDefault(u => u.username.Equals(nonRegisterdUser.username));
+            if(existingUser is not null )
+            {
+                mappedActiveUsers.Remove(existingUser);
+            }
             mappedActiveUsers.Add(nonRegisterdUser);
             return true;
         }
@@ -32,6 +37,15 @@ public class UserConnectionMapper
         if (user is null){return null;}
 
         return user.connectionId; 
+    }
+
+    public void RemoveUser(string? userName)
+    {
+        var user = mappedActiveUsers.FirstOrDefault(u => u.connectionId is not null &&  u.username.Equals(userName));
+        if (user is not null)
+        {
+            mappedActiveUsers.Remove(user);
+        }
     }
     
 }
